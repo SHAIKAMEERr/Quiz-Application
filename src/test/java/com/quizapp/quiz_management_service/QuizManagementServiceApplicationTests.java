3b898bf4f46f115806f5c1fd751afc7b0cee11b5
SHAIK AMEER <shaikameerjann@gmail.com>
2025-01-06 12:25:05 +0530

    Quiz Management Service

---
+++ src/test/java/com/quizapp/quiz_management_service/QuizManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizapp.quiz_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
